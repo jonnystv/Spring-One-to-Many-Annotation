b15e66fd5d5e6ee1145e772eb84ef19f9f37540e
jonnystv <jonathan@darefoundation.com>
2021-02-23 21:54:31 +0000

    repositories created

--- src/main/java/com/codeclan/example/Spring_Annotation/repositories/FolderRepository.java
+++ src/main/java/com/codeclan/example/Spring_Annotation/repositories/FolderRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.Spring_Annotation.repositories;
 
-public interface FolderRepository {
+import com.codeclan.example.Spring_Annotation.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
