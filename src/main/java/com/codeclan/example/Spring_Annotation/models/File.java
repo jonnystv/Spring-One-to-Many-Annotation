10609e38ffd0f0a712f87613bed55e016cf3ce39
jonnystv <jonathan@darefoundation.com>
2021-02-23 21:49:46 +0000

    models created

--- src/main/java/com/codeclan/example/Spring_Annotation/models/File.java
+++ src/main/java/com/codeclan/example/Spring_Annotation/models/File.java
@@ -1,4 +1,52 @@
 package com.codeclan.example.Spring_Annotation.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "files")
 public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file")
+    private String name;
+
+    @Column(name = "file_size")
+    private int fileSize;
+
+    public File(Long id, String name, int fileSize) {
+        this.id = id;
+        this.name = name;
+        this.fileSize = fileSize;
+    }
+
+    public File(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
 }
--- src/main/java/com/codeclan/example/Spring_Annotation/models/Folder.java
+++ src/main/java/com/codeclan/example/Spring_Annotation/models/Folder.java
@@ -1,4 +1,50 @@
 package com.codeclan.example.Spring_Annotation.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "files")
+    private List<File> files;
+
+    public Folder(Long id, String name, List<File> files) {
+        this.id = id;
+        this.name = name;
+        this.files = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
