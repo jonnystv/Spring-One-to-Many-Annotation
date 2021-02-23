2ea619c77b334222e3ecaca1b048b7d2833635e9
jonnystv <jonathan@darefoundation.com>
2021-02-23 23:39:47 +0000

    getAllUsers route created

--- src/main/java/com/codeclan/example/Spring_Annotation/controllers/UserController.java
+++ src/main/java/com/codeclan/example/Spring_Annotation/controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.codeclan.example.Spring_Annotation.controllers;
 
+import com.codeclan.example.Spring_Annotation.models.User;
+import com.codeclan.example.Spring_Annotation.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
 }
