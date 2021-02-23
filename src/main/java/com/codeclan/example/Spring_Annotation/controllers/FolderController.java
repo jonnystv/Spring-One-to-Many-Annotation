cd932bd5fc95ab3262c9540435657a30645b5fef
jonnystv <jonathan@darefoundation.com>
2021-02-23 23:54:11 +0000

    getFolder and getFile route created

--- src/main/java/com/codeclan/example/Spring_Annotation/controllers/FolderController.java
+++ src/main/java/com/codeclan/example/Spring_Annotation/controllers/FolderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class FolderController {
     @GetMapping(value = "/folders")
     public ResponseEntity<List<Folder>> getAllFolders() {
         return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity getFolder(@PathVariable Long id) {
+        return new ResponseEntity(folderRepository.findById(id),  HttpStatus.OK);
     }
 }
