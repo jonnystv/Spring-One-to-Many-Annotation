250a9b26f6c9066ac09f43e9ac8d924f29d0513b
jonnystv <jonathan@darefoundation.com>
2021-02-24 00:35:32 +0000

    postFolder and postFile created dont work postUser broken

--- src/main/java/com/codeclan/example/Spring_Annotation/controllers/FileController.java
+++ src/main/java/com/codeclan/example/Spring_Annotation/controllers/FileController.java
@@ -5,9 +5,7 @@ import com.codeclan.example.Spring_Annotation.repositories.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,11 @@ public class FileController {
     @GetMapping(value = "/files/{id}")
     public ResponseEntity getFile(@PathVariable Long id) {
         return new ResponseEntity(fileRepository.findById(id),  HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity postFile (@RequestBody File file) {
+        fileRepository.save(file);
+        return new ResponseEntity(file, HttpStatus.CREATED);
     }
 }
